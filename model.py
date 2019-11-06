73fd41c1c2bea16024714c6c56eec74a56757dd9
srahi <45075189+srahi@users.noreply.github.com>
2019-11-06 16:14:58 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,11 @@
 import pandas as pd
-from sklearn.impute import SimpleImputer
+
 from scipy.stats.mstats import winsorize
 
-df= pd.read_csv('dataset.csv',header=None, names=["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal", "num"], na_values=['?'])
+df= pd.read_csv('dataset.csv',header=None, names=["age", "sex", "cp", "trestbps", 
+                                                  "chol", "fbs", "restecg", "thalach", 
+                                                  "exang", "oldpeak", "slope", "ca", 
+                                                  "thal", "num"], na_values=['?'])
 df = df.replace('?','NaN')
 
 df['ca']=df['ca'].astype(float)
@@ -11,48 +14,53 @@ df['thal']=df['thal'].astype(float)
 df_m=df.fillna(df.mean())
 
 
+
 df_m['ca'] = pd.to_numeric(df_m['ca'], errors='coerce')
 df_m[['age', 'sex', 'fbs', 'exang', 'ca']] = df_m[['age', 'sex', 'fbs', 'exang', 'ca']].astype(int)
 df_m[['trestbps', 'chol', 'thalach', 'oldpeak']] = df_m[['trestbps', 'chol', 'thalach', 'oldpeak']].astype(float)
 df_m['num'].replace(to_replace=[1, 2, 3, 4], value=1, inplace=True)
+
+
+
 import seaborn as sns
 sns.boxplot(x=df_m['ca'])
-
 
 df_m['Ca']=winsorize(df_m['ca'],limits=[0.0,0.25])
 df_m.drop("ca", axis=1, inplace=True) 
 sns.boxplot(x=df_m['Ca'])
 
 sns.boxplot(x=df_m['chol'])
+
 df_m['Chol']=winsorize(df_m['chol'],limits=[0.0,0.25])
 sns.boxplot(x=df_m['Chol'])
 df_m.drop("chol", axis=1, inplace=True) 
 
-
 sns.boxplot(x=df_m['oldpeak'])
+
 df_m['Oldpeak']=winsorize(df_m['oldpeak'],limits=[0.03,0.05])
 sns.boxplot(x=df_m['Oldpeak'])
 df_m.drop("oldpeak", axis=1, inplace=True) 
 
-
 #Box Plot
 sns.boxplot(x=df_m['trestbps'])
+
 # Winsorization
 df_m['Trestbps']=winsorize(df_m['trestbps'],limits=[0.0,0.25])
 sns.boxplot(x=df_m['Trestbps'])
 df_m.drop("trestbps", axis=1, inplace=True) 
 
 sns.boxplot(x=df_m['thal'])
+
 df_m['Thal']=winsorize(df_m['thal'],limits=[0.03,0.05])
 sns.boxplot(x=df_m['Thal'])
 df_m.drop("thal", axis=1, inplace=True) 
 
+
 sns.boxplot(x=df_m['thalach'])
+
 df_m['Thalach']=winsorize(df_m['thalach'],limits=[0.03,0.05])
 sns.boxplot(x=df_m['Thalach'])
 df_m.drop("thalach", axis=1, inplace=True) 
-
-
 
 import matplotlib.pyplot as plt
 
@@ -81,17 +89,13 @@ clf = clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
-## Accuracy: 0.7912087912087912
-
-
-
-
-
-
 
 #### Logistic Regression ####
 
+
 from sklearn.linear_model import LogisticRegression 
+
+
 feature_cols = ["age", "sex", "cp", "Trestbps", "Chol", "fbs", "restecg", "Thalach", "exang", "Oldpeak", "slope", "Ca", "Thal"]
 x = df_m[feature_cols] 
 y = df_m.num 
@@ -107,25 +111,20 @@ xtest = sc_x.transform(xtest)
 
 #print (xtrain[0:10, :]) 
 
+
+
 classifier = LogisticRegression(random_state = 0) 
 classifier.fit(xtrain, ytrain)
 
 y_pred = classifier.predict(xtest) 
 
 from sklearn.metrics import confusion_matrix 
-cm = confusion_matrix(ytest, y_pred)   
+cm = confusion_matrix(ytest, y_pred) 
+  
+print ("Confusion Matrix : \n", cm) 
 
-#print ("Confusion Matrix : \n", cm) 
 from sklearn.metrics import accuracy_score 
 print ("Accuracy : ", accuracy_score(ytest, y_pred))
-
-## Accuracy :  0.8289473684210527
-
-
-
-
-
-
 
 
 #### Random Forest ####
@@ -141,7 +140,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 from sklearn.ensemble import RandomForestClassifier
 
 #Create a Gaussian Classifier
-clf=RandomForestClassifier(n_estimators=100)
+clf=RandomForestClassifier(n_estimators=150)
 
 #Train the model using the training sets y_pred=clf.predict(X_test)
 clf.fit(X_train,y_train)
@@ -153,24 +152,17 @@ from sklearn import metrics
 # Model Accuracy, how often is the classifier correct?
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
-## Accuracy: 0.7582417582417582
-
-
-
-
-
-
-
-
-
 
 #### Naive Bayes####
 # Import LabelEncoder
 from sklearn import preprocessing
 
+
+
 feature_cols = ["age", "sex", "cp", "Trestbps", "Chol", "fbs", "restecg", "Thalach", "exang", "Oldpeak", "slope", "Ca", "Thal"]
 X = df_m[feature_cols] 
 y = df_m.num 
+
 
 # Import train_test_split function
 from sklearn.model_selection import train_test_split
@@ -178,6 +170,8 @@ from sklearn.model_selection import train_test_split
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3,random_state=109) # 70% training and 30% test
 #Import Gaussian Naive Bayes model
+
+
 from sklearn.naive_bayes import GaussianNB
 
 #Create a Gaussian Classifier
@@ -187,16 +181,14 @@ model = GaussianNB()
 model.fit(X_train,y_train)
 
 #Predict Output
-y_pred= model.predict(X_test) 
+y_pred= model.predict(X_test) # 0:Overcast, 2:Mild
 #print("Predicted Value:", y_pred)
+
 
 #Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
 
 # Model Accuracy, how often is the classifier correct?
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-
-
-## Accuracy: 0.7802197802197802
 
 
